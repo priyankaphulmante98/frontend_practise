5f2ff3f621d1d30fd9cdd0913cab07d55014b30d
priyanka <priyankaphulmante98@gmail.com>
2023-02-06 16:08:00 +0530

    123

--- javascript-fect-map-mediaQuery/user-2/index.js
+++ javascript-fect-map-mediaQuery/user-2/index.js
@@ -1,1 +1,13 @@
 // https://fakestoreapi.com/products
+async function getData(){
+    let res = await fetch(`https://fakestoreapi.com/products`)
+    let data = await res.json();
+    console.log(data)
+    await new Promise(resolve => 
+        setTimeout(resolve, 4000))
+     console.log('hello')
+
+    }
+    getData()
+  
+    
